b37cc9469784627561348ac42494a6bf7acc0cb4
M4RIAGABRIELAMILANI <mariagabriela5024@gmail.com>
2026-05-31 22:30:47 -0300

    Adiciona script do banco de dados

--- database/receitas_db (1).sql
+++ database/receitas_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24/05/2026 às 03:59
+-- Tempo de geração: 01/06/2026 às 03:27
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.0.30
 
@@ -41,7 +41,7 @@ CREATE TABLE `receitas` (
 --
 
 INSERT INTO `receitas` (`id`, `titulo`, `descricao`, `modo_preparo`, `usuario_id`, `created_at`) VALUES
-(1, 'Bolos', 'rggre', 'erethth', 1, '2026-05-24 01:26:23');
+(2, 'Bolo Simples', 'Bolo simples com poucos ingredientes', 'Ingredientes (12 porções)\r\naçúcar\r\n2 xícaras (chá) de açúcar\r\nfarinha de trigo\r\n3 xícaras (chá) de farinha de trigo\r\nmargarina\r\n4 colheres (sopa) de margarina\r\novo\r\n3 ovos\r\nleite\r\n1 e 1/2 xícara (chá) de leite\r\nfermento em pó químico\r\n1 colher (sopa) bem cheia de fermento em pó\r\nModo de preparo\r\nModo de preparo : 40min\r\n1\r\nBata as claras em neve e reserve.\r\n\r\n2\r\nMisture as gemas, a margarina e o açúcar até obter uma massa homogênea.\r\n\r\n3\r\nAcrescente o leite e a farinha de trigo aos poucos, sem parar de bater.\r\n\r\n4\r\nPor último, adicione as claras em neve e o fermento.\r\n\r\n5\r\nDespeje a massa em uma forma grande de furo central untada e enfarinhada.\r\n\r\n6\r\nAsse em forno médio 180 °C, preaquecido, por aproximadamente 40 minutos ou ao furar o bolo com um garfo, este saia limpo.', 1, '2026-05-31 23:54:47');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `created_at`) VALUES
-(1, 'MARIA MILANI', 'mariagabriela5024@gmail.com', '$2y$10$q0yHmaea0G4ce9W7xivYWOavXPkt3YgIc.E0/e9ZURniQz4GjEoQy', '2026-05-23 00:39:20');
+(1, 'MARIA MILANI', 'mariagabriela5024@gmail.com', '$2y$10$q0yHmaea0G4ce9W7xivYWOavXPkt3YgIc.E0/e9ZURniQz4GjEoQy', '2026-05-23 00:39:20'),
+(2, 'Maria Linda', 'marialinda@gmail.com', '$2y$10$O4/Duw/B4iQsvmH9sUO36.bsxqE.p0a1BZZ0CkTs40GQ8SIsZfx0e', '2026-06-01 00:27:50'),
+(6, 'Maria Linda', 'marialinda2@gmail.com', '$2y$10$R1gXAE14eXw8q63V0cpXKuPam0ioHMWfN/vBhY21DQ5EY/KQfQOBm', '2026-06-01 00:32:10');
 
 --
 -- Índices para tabelas despejadas
@@ -90,13 +92,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `receitas`
 --
 ALTER TABLE `receitas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
